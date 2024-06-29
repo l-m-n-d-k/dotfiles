302fe1b7a33f542d64783d99539c108edf450a1e
Dokukin Dmitry <72608150+l-m-n-d-k@users.noreply.github.com>
2024-06-30 01:08:30 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,21 +3,19 @@
 # Обновление системы и установка пакетов
 sudo pacman -Syu
 
-sudo pacman -S git firefox telegram-desktop btop fish neovim tmux openssh ncdu sxiv zathura bat discord 
+sudo pacman -S git github-cli firefox telegram-desktop btop fish neovim tmux openssh ncdu sxiv zathura bat discord xclip
 
 # Изменение оболочки на fish
 chsh -s /usr/bin/fish
 
 # Создание SSH ключа для GitHub
-ssh-keygen -t ed25519 -C "dima2642007@gmail.com" -f ~/.ssh/github_id_ed25519 -N ""
-echo "SSH ключ для GitHub создан. Скопируйте следующий ключ и добавьте его в ваш GitHub аккаунт:"
-cat ~/.ssh/github_id_ed25519.pub
+# ssh-keygen -t ed25519 -C "l-m-n-d-k@GitHub" -f ~/.ssh/github_id_ed25519 -N ""
 
 # Установка yay для работы с AUR
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ..
-rm -rf yay
+# git clone https://aur.archlinux.org/yay.git
+# cd yay
+# makepkg -si
+# cd ..
+# rm -rf yay
 
 echo "Установка и настройка завершены!"
