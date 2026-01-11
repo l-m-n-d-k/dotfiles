f5ee403919d46668b0b6466e08bac8b22c9a5c4e
l-m-n-d-k <dima2642007@gmail.com>
2026-01-11 20:09:23 +0300

    add some cfg

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    alias ls="exa"
+    wallust run projects/syncthing/customs/wallpaper/trees.png
+    clear
+end
+
+fish_add_path /home/lmndk/.spicetify
+
+# Generated for envman. Do not edit.
+test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
